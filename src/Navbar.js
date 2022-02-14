e37e395bc870311ac1533671c9a67200883dd156
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-02-14 16:29:36 +0500

    react blog , completed props

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The React Blog</h1>
+            <div className="llinks">
+                <a href="/">Home</a>
+                <a href="/create" >New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
