98f160321555f532815c7b944310d494a974cb5f
UsamaHussain000111 <usamahussaincs@gmail.com>
2022-02-24 23:56:14 +0500

    Route parameters

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router , Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
-
-  const title = 'Welcome to the new blog';
-
   return (
     <Router>
       <div className="App">
@@ -16,8 +14,11 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/create">
+            <Route path="/create">
               <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,15 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const { data: blogs , isPending , error } = useFetch('http://localhost:8000/blogs');
-
-    return (
-        <div className="home">
-            { error && <div> { error } </div> }
-            { isPending && <div>Loading..</div> }
-            { blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
-        </div>
-    );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs" /> }
+    </div>
+  );
 }
  
 export default Home;
